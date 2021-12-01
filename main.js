82d7fd3a1ca9569666db96a25c2a8fa643eabb95
Mughilavan <83823599+Mughilvan@users.noreply.github.com>
2021-12-01 19:51:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+music="";
+function preload(){
+    song=loadSound("music1.mp3");
+    song=loadSound("music2.mp3");
+}
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video,0,0,600,500);
+}
+function play(){
+    music.play();
+}
